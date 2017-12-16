fb7b07a1c3062acdb6290e3d7ce9cb6a0ff58b2c
Eugene Gavrilov <eugene.gavrilov@pubnative.net>
2017-12-16 22:52:04 +0100

    Replace find with aggregation for better projection and filtering of mongo requests. Also Helmet and CORS added.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var cors = require('cors');
+var helmet = require('helmet');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,6 +20,8 @@ app.set('view engine', 'vash');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
+app.use(helmet())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/nutrients.js
+++ routes/nutrients.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoConnect = require('../utils/mongoConnect');
 var router = express.Router();
+const projection = { fields: { name: 1, code: 1, units: 1, max: 1, _id: 0, nutrients: 1 } };
 
-router.get('/', async function(req, res) {
-  const products = await getProducts();
+
+router.get('/', async function (req, res) {
+  const products = await getProducts(req.query);
   res.send(products);
 });
 
@@ -12,14 +14,38 @@ router.get('/list', async (req, res) => {
   res.send(nutrients);
 });
 
-async function getProducts() {
+async function getProducts(params) {
+  const find = {}
+  const shortNutrientsList = params.id ? { $in: ['$$n.code', ['203', '204', '205']] } : {}
+  const filterConditions = {
+    input: '$nutrients',
+    as: 'n',
+    cond: {
+      $and: [
+        { $ne: ['$$n.value', 0] },
+        { ...shortNutrientsList }
+      ]
+    }
+  }
+  if (params.filter) {
+    find.$or = [
+      { 'name.long': { $regex: params.filter, $options: 'i' } },
+      { 'name.long_ru': { $regex: params.filter, $options: 'i' } }
+    ]
+  }
   const collection = await mongoConnect.read.then(_db => _db.collection('nutrients'));
-  const products = await collection.find({}).limit(10).toArray();
+  const products = await collection.aggregate([{
+    $project: {
+      name: 1,
+      nutrients: { $filter: filterConditions }
+    }
+  },
+  { $match: { ...find } },
+  { $limit: 10 }]).toArray()
   return products;
 }
 
 async function getNutrients() {
-  const projection = { fields: { name: 1, code: 1, units: 1, max: 1, _id: 0 }};
   const collection = await mongoConnect.read.then(_db => _db.collection('statistics'));
   const nutrients = await collection.find({}, projection).toArray();
   return nutrients;
