54e042f6713f72fa9c1e006a9ec5e619c7875d93
Eugene Gavrilov <eugene.gavrilov@pubnative.net>
2017-12-18 02:58:34 +0100

    Add get by id

--- routes/nutrients.js
+++ routes/nutrients.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoConnect = require('../utils/mongoConnect');
 var router = express.Router();
+const ObjectId = require('mongodb').ObjectId;
 const projection = { fields: { name: 1, code: 1, units: 1, max: 1, _id: 0, nutrients: 1 } };
 
 
@@ -14,16 +15,21 @@ router.get('/list', async (req, res) => {
   res.send(nutrients);
 });
 
+router.get('/:id', async (req, res) => {
+  const product = await getProducts(ObjectId(req.params.id.toString()));
+  res.send(product);
+});
+
 async function getProducts(params) {
   const find = {}
-  const shortNutrientsList = params.id ? { $in: ['$$n.code', ['203', '204', '205']] } : {}
+  // const shortNutrientsList = params.id ? { $in: ['$$n.code', ['203', '204', '205']] } : {}
   const filterConditions = {
     input: '$nutrients',
     as: 'n',
     cond: {
       $and: [
         { $ne: ['$$n.value', 0] },
-        { ...shortNutrientsList }
+        // { ...shortNutrientsList }
       ]
     }
   }
@@ -33,6 +39,11 @@ async function getProducts(params) {
       { 'name.long_ru': { $regex: params.filter, $options: 'i' } }
     ]
   }
+
+  if (params.id) {
+    find._id = ObjectId(params.id.toString())
+  }
+  
   const collection = await mongoConnect.read.then(_db => _db.collection('nutrients'));
   const products = await collection.aggregate([{
     $project: {
